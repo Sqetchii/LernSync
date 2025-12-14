cd04bb72b88eea71f8addb8569fc7d4d6267d287
sqetchii <nicolastraenkner@gmail.com>
2025-12-14 21:53:25 +0100

    enhance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import configparser
+from pathlib import Path
 from webdav3.client import Client
 from webdav3.exceptions import ResponseErrorCode
 from credential_service import get_credentials
@@ -17,24 +18,34 @@ local_path = config['webdav']['local_path']
 service = config['webdav']['service']
 use_skipables = config['webdav'].getboolean('use_skipables')
 
-while True:
-    username, password = get_credentials(service)
+def main():
+    if use_skipables:
+        try:
+            Path('skipables.txt').open('x', encoding='utf-8').close()
+        except FileExistsError:
+            pass
 
-    options = {
-        'webdav_hostname': f'{host_name}',
-        'webdav_login': f'{username}',
-        'webdav_password': f'{password}'
-    }
-    try:
-        client = Client(options)
-        client.webdav.disable_check = True
+    while True:
+        username, password = get_credentials(service)
 
-        print('Erfolgreicher Login.')
-        pull_continue(client, remote_path, local_path, use_skipables)
-        break
-    except ResponseErrorCode as e:
-        if getattr(e, 'code', None) == 401:
-            log.error(f'Fehlercode: {e.code}, Benutzername oder Passwort sind falsch.')
-        else:
-            log.error(f'Fehlercode: {e.code}, Unbekanntes Problem bei der Anmeldung.')
-        delete_entry(service, username)
+        options = {
+            'webdav_hostname': f'{host_name}',
+            'webdav_login': f'{username}',
+            'webdav_password': f'{password}'
+        }
+        try:
+            client = Client(options)
+            client.webdav.disable_check = True
+
+            print('Erfolgreicher Login.')
+            pull_continue(client, remote_path, local_path, use_skipables)
+            break
+        except ResponseErrorCode as e:
+            if getattr(e, 'code', None) == 401:
+                log.error(f'Fehlercode: {e.code}, Benutzername oder Passwort sind falsch.')
+            else:
+                log.error(f'Fehlercode: {e.code}, Unbekanntes Problem bei der Anmeldung.')
+            delete_entry(service, username)
+
+if __name__ == '__main__':
+    main()
